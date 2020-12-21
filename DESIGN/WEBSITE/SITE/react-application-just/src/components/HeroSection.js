20cf3b11e482bf69aa30ef6bc19d3c6e8fb6d11d
Samuel-Kowatschek <samkow@gmx.at>
2020-12-21 12:22:29 +0100

    added hero component

---
+++ DESIGN/WEBSITE/SITE/react-application-just/src/components/HeroSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+import {Button} from "./Button";
+import './HeroSection.css';
+
+function HeroSection(){
+    return (
+        <div className='hero-container'>
+            <h1>Willkommen!</h1>
+            <div className='hero-btns'>
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn-large'> LETS GO</Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn-large'>BUBUBUBU</Button>
+                asjdkölfaösjdklfjö
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
