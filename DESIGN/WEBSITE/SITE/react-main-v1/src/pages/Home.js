ea0467776b973a83d454eff56aded79d7add5c4f
Samuel-Kowatschek <samkow@gmx.at>
2021-01-05 12:44:52 +0100

    created and edited pages, landingpage almost complete

---
+++ DESIGN/WEBSITE/SITE/react-main-v1/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../css/home.css'
+import {Button} from '../components/Button';
+import * as AiIcons from 'react-icons/ai';
+import * as HiIcons from 'react-icons/hi';
+
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Willkommen!</h1>
+            <div className='button-area'>
+                <Button onClick={() => { window.location.href = "todo"}}><HiIcons.HiOutlineClipboardList /></Button>
+                <Button onClick={() => {window.location.href = "projects"}}><AiIcons.AiFillProject /></Button>
+                <Button onClick={() => {window.location.href = "calendar"}}><HiIcons.HiCalendar/></Button>
+                <Button onClick={() => {window.location.href = "settings"}}><AiIcons.AiFillSetting/></Button>
+
+            </div>
+            <h1>Anstehende Tasks</h1>
+            <h1>Anstehende Termine</h1>
+        </div>
+    );
+}
+
+export default Home;
+
---
+++ DESIGN/WEBSITE/SITE/react-main-v1/src/pages/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/todo.css';
+import {Button} from "../components/Button";
+import * as HiIcons from "react-icons/hi";
+import * as AiIcons from "react-icons/ai";
+
+const Todo = () => {
+    return (
+        <div className='todo'>
+            <h1>TODOS</h1>
+        </div>
+    );
+};
+
+export default Todo;
