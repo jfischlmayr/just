f2ebe3c1d0e6e542d3bd64185f5658e46dd7544e
Samuel-Kowatschek <samkow@gmx.at>
2021-01-05 12:59:12 +0100

    created basic css file for each page

---
+++ DESIGN/WEBSITE/SITE/react-main-v1/src/pages/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/settings.css';
+import '../css/pages.css';
+
+
+const Settings = () => {
+    return (
+        <div className='settings'>
+            <h1>Settings</h1>
+        </div>
+    );
+};
+
+export default Settings;
