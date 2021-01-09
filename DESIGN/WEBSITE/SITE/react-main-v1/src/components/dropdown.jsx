858fb0130e04c4305c3bcd324c89e95c997a20f3
Samuel-Kowatschek <samkow@gmx.at>
2021-01-09 13:34:50 +0100

    further improved dropdown component

--- DESIGN/WEBSITE/SITE/react-main-v1/src/components/dropdown.jsx
+++ DESIGN/WEBSITE/SITE/react-main-v1/src/components/dropdown.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import '../css/dropdown.css';
 
-const Dropdown = ({projects}) => {
+const Dropdown = ({options, prompt, value, onChange}) => {
+
+    const [open, setOpen] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+       document.addEventListener("click", close);
+       return () => document.removeEventListener("click", close);
+    }, []);
+
+    function close(e){
+        setOpen(e && e.target === ref.current);
+    }
+
     return (
         <div className='dropdown'>
-            <div className='control'>
-                <div className = 'selected-value'>Select Project...</div>
-                <div className='arrow'/>
+            <div className='control' onClick={() => setOpen(prev => !prev)}>
+                <div className = 'selected-value' ref={ref}>
+                    {value ? value.name : prompt}
+                </div>
+                <div className={`arrow ${open ? "open" : null}`}/>
             </div>
-            <div className='options'>
+            <div className={`options ${open ? "open" : null}`}>
                 {
-                    projects.map((project) => (
-                        <div className='option'>{project.projectname}</div>
+                    options.map((option) => (
+                        <div className={`option ${value === option ? "selected" : null}`} onClick={() => {
+                            onChange(option);
+                            setOpen(false);
+                        }}>{option.name}</div>
                     ))}
             </div>
         </div>
--- DESIGN/WEBSITE/SITE/react-main-v1/src/pages/Todo.js
+++ DESIGN/WEBSITE/SITE/react-main-v1/src/pages/Todo.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Dropdown from "../components/dropdown";
 import projects from '../json test data/projects.json'
 
 const Todo = () => {
+
+    const [value, setValue] = useState(null)
+
     return (
         <div className='todo'>
             <h1>TODOS</h1>
             <div style={{width: 200}}>
-                <Dropdown projects={projects}/>
+                <Dropdown
+                    prompt='Select project... '
+                    options={projects}
+                    value={value}
+                    onChange={val => setValue(val)}
+                />
+
             </div>
         </div>
     );
