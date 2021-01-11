e6b767e351861dfc79f5d92be2ebe7a0accb0100
Samuel-Kowatschek <samkow@gmx.at>
2021-01-11 12:33:33 +0100

    added the todolist component, which is basically the todolist

---
+++ DESIGN/WEBSITE/SITE/react-main-v1/src/components/TodoListComponent.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import TodoForm from "./TodoForm";
+import '../css/TodoListComponent.css';
+
+const TodoListComponent = () => {
+    return (
+        <div className='todoList'>
+        </div>
+    );
+};
+
+export default TodoListComponent;
